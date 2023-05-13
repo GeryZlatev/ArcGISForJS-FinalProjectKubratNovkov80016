8ab52f0d491180a213dbae4d2175d53cef3bf096
Gergana Zlateva <gery.zlatev@gmail.com>
2023-05-13 20:07:52 +0300

    bugfixes;

--- map.js
+++ map.js
@@ -1,109 +1,109 @@
 require([
-    "esri/config",
-    "esri/WebMap",
-    "esri/views/MapView",
-    "esri/widgets/ScaleBar",
-    "esri/widgets/Legend",
-    "esri/widgets/Home",
-    "esri/widgets/LayerList",
-    "esri/widgets/BasemapToggle",
-    "esri/widgets/BasemapGallery",
-    "esri/widgets/Search"
-  ], function(esriConfig, WebMap, MapView, ScaleBar, Legend, Home, LayerList, BasemapGallery, BasemapToggle, Search) {
-    esriConfig.ariKey = "AAPK7b185bfbfd6649c6b7c7f2be20b7f9c45vp0RIxhE9-cmNKlLn8Aq8YsEG5gdEQh8poOkNPnEi8xD090fh8ogv8fAAfBG3f-";
+  "esri/config",
+  "esri/WebMap",
+  "esri/views/MapView",
+  "esri/widgets/ScaleBar",
+  "esri/widgets/Legend",
+  "esri/widgets/Home",
+  "esri/widgets/LayerList",
+  "esri/widgets/BasemapToggle",
+  "esri/widgets/BasemapGallery",
+  "esri/widgets/Search"
+], function (esriConfig, WebMap, MapView, ScaleBar, Legend, Home, LayerList, BasemapToggle, BasemapGallery, Search) {
+  esriConfig.ariKey = "AAPK7b185bfbfd6649c6b7c7f2be20b7f9c45vp0RIxhE9-cmNKlLn8Aq8YsEG5gdEQh8poOkNPnEi8xD090fh8ogv8fAAfBG3f-";
 
-    const webMap = new WebMap ({
-        portalItem: {
-            id: "04ae1a687e874bf0a4b4a014c5582acb"
-        } 
-      })
+  const webMap = new WebMap({
+    portalItem: {
+      id: "04ae1a687e874bf0a4b4a014c5582acb"
+    }
+  })
 
-      const view = new MapView({
-        container: "viewDiv",
-        map: WebMap
-      });
+  const view = new MapView({
+    container: "viewDiv",
+    map: webMap //WebMap is incorect. The variable name is webMap
+  });
 
-      const homeBtn = Home({
-        view: view
-      })
- 
-      view.ui.add(homeBtn, "top-left");
+  const homeBtn = new Home({ //missing new key word!
+    view: view
+  })
 
-      const legend = new Legend ({
-        view: view
-      });
+  view.ui.add(homeBtn, "top-left");
 
-      view.ui.add(legend, "bottom-left");
+  const legend = new Legend({
+    view: view
+  });
 
-      const scaleBar = new ScaleBar({
-        view: view,
-        unit: "metric",
-        style: "ruler"
-      });
+  view.ui.add(legend, "bottom-left");
 
-      view.ui.add(scaleBar, "bottom-right");
+  const scaleBar = new ScaleBar({
+    view: view,
+    unit: "metric",
+    style: "ruler"
+  });
 
-      view.ui.add("layerList-btn", "top-right");
+  view.ui.add(scaleBar, "bottom-right");
 
-      const layerList = new LayerList({
-        view: view 
-      })
+  view.ui.add("layerList-btn", "top-right");
 
-      const searchWidget = new Search({
-        view:view
-      })
+  const layerList = new LayerList({
+    view: view
+  })
 
-      view.ui.add(searchWidget, "top-left");
+  const searchWidget = new Search({
+    view: view
+  })
 
-      const basemapToggle = new BasemapToggle({
-        view: view,
-        nextbasemap: "arcgis-imagery"
-      })
-      
-      const basemapGallery = new BasemapGallery({
-        view: view,
-        source: {
-            query: {
+  view.ui.add(searchWidget, "top-left");
 
-            }
-        },
-      })
+  const basemapToggle = new BasemapToggle({
+    view: view,
+    nextbasemap: "arcgis-imagery"
+  })
 
-      view.ui.add(basemapGallery, "top-right");
+  const basemapGallery = new BasemapGallery({
+    view: view,
+    source: {
+      query: {
 
-      view.ui.add(basemapToggle, "bottom-right");
+      }
+    },
+  })
 
-      view.ui.add(layerList-btn, "top-right");
-      view.ui.add(basemap-btn, "top-right")
+  view.ui.add(basemapGallery, "top-right");
 
-      document
-      .getElementById("layerList-btn")
-    .addEventListener("click", function() {
-        toggleButton("layerLiat")
+  view.ui.add(basemapToggle, "bottom-right");
+
+  view.ui.add("layerList-btn", "top-right"); // layerList - btn is incorrect. It must be with quotes and without white space;
+  view.ui.add("basemap-btn", "top-right"); //basemap - btn is incorrect. It must be with quotes and without white space;
+
+  document
+    .getElementById("layerList-btn")
+    .addEventListener("click", function () {
+      toggleButton("layerList") //layerLiat - syntax error
     })
 
-    function toggleButton (item){
-        const layerListEl = document.getElementsByClassName("esri-layer-list")[0];
-        const galleryEl = document.getElementsByClassName("esri-basemap-gallery")[0];
-        let currentProp;
+  function toggleButton(item) {
+    const layerListEl = document.getElementsByClassName("esri-layer-list")[0];
+    const galleryEl = document.getElementsByClassName("esri-basemap-gallery")[0];
+    let currentProp;
 
-        
 
-        if(item == "layerlist"){
-            currentProp = layerListEl.style.getPropertyValue("display");
-            layerListEl.style.setProperty("display", currentProp == "block" ? "none" : "block");
-            galleryEl.style.setProperty("display", "none");
-        }else if(item == "gallery"){
-            currentProp = galleryEl.style.getPropertyValue("display");
-            galleryEl.style.setProperty("display", currentProp == "block" ? "none" : "block");
-            layerListEl.style.setProperty("display", "none");
-        }
+
+    if (item == "layerlist") { // layerlist ...list must be upper case
+      currentProp = layerListEl.style.getPropertyValue("display");
+      layerListEl.style.setProperty("display", currentProp == "block" ? "none" : "block");
+      galleryEl.style.setProperty("display", "none");
+    } else if (item == "gallery") {
+      currentProp = galleryEl.style.getPropertyValue("display");
+      galleryEl.style.setProperty("display", currentProp == "block" ? "none" : "block");
+      layerListEl.style.setProperty("display", "none");
     }
+  }
 
-    document
+  document
     .getElementById("basemap-btn")
-    .addEventListener("click",function() {
-        toggleButton("gallery")
-    } )
+    .addEventListener("click", function () {
+      toggleButton("gallery")
+    })
 
-   })
+})
